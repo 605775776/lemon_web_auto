758f937427b9e18d629310c7245419e28843b040
605775776 <605775776@qq.com>
2020-12-05 00:26:40 +0800

    2

--- PO/PageLocators/index_page_locs.py
+++ PO/PageLocators/index_page_locs.py
@@ -4,4 +4,13 @@ from selenium.webdriver.common.by import By
 class IndexPageLocs:
     # 用户名元素 -是否存在状态
     exit_link = (By.XPATH, "//a[text()='退出']")
-    bid_button = (By.XPATH, "//a[@class='btn btn-special']")
+
+    # 关于我们
+    about_us = (By.XPATH, '//a[text()="关于我们"]')
+    # 用户昵称
+    user_link = (By.XPATH, '//a[@href="/Member/index.html"]')
+
+    # 投标按钮 默认第一个
+    # bid_button = (By.XPATH, "//a[@class='btn btn-special']")
+    # 第二个 找到分支 或者用find_elements找到全部再用下标
+    bid_button = (By.XPATH, '//div[@class="b-unit"][2]//a[text()="抢投标"]')
--- PO/PageLocators/user_page_locs.py
+++ PO/PageLocators/user_page_locs.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 
 class UserPageLocs:
-
+    # 可用余额
+    user_leftMoney = (By.XPATH, '//li[@class="color_sub"]')
 
