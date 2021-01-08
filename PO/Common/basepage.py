0035874089bbb31d212d0cd2b9c0561709533073
605775776 <605775776@qq.com>
2021-01-08 12:48:34 +0800

    0108

--- PO/Common/basepage.py
+++ PO/Common/basepage.py
@@ -2,11 +2,13 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import logging
-class BasePage:
 
+
+class BasePage:
 
     def __init__(self, driver: WebDriver):
         self.driver = driver
+
     # 等待可见
     def wait_ele_visible(self, loc, img_name, timeout=20, poll_fre=0.5):
         logging.info("{}等待{}元素可见".format(img_name, loc))
@@ -18,9 +20,10 @@ class BasePage:
             self.save_page_shot(img_name)
             logging.exception("等待元素可见失败：")
             raise
-        else：
+        else:
             # 时间差
             pass
+
     # 查找元素
     def get_element(self):
         pass
@@ -41,14 +44,14 @@ class BasePage:
     def get_ele_text(self):
         pass
 
-
     def save_page_shot(self, img_name):
         # 存储图片
         # 命名规范 页面名称_页面行为_时间.png
         # 文件完整名称 = Outputs的screenshots+页面名称_页面行为_时间.png
-        file_name ="{}_{}.png".format(img_name, "当前时间")
+        file_name = "{}_{}.png".format(img_name, "当前时间")
         self.driver.save_screenshot("图片存储路径" + img_name)
         logging.info("页面图片保存在：{}".format("图片存储路径" + img_name))
+
 
 if __name__ == '__main__':
     pass
