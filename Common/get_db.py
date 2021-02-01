ad47200c0c723cbb137275601804024f9a7558c4
605775776@qq.com <605775776@qq.com>
2021-02-01 18:51:12 +0800

    0201gitignore

---
+++ Common/get_db.py
@@ -0,0 +1,12 @@
+from Common.db_handler import DBHandler
+from config.yaml_handler import yaml_data
+
+
+class MyDBHandler(DBHandler):
+    def __init__(self):
+        super().__init__(host=yaml_data['database']['host'],
+                         port=yaml_data['database']['port'],
+                         user=yaml_data['database']['user'],
+                         password=yaml_data['database']['password'],
+                         charset=yaml_data['database']['charset'],
+                         database=yaml_data['database']['database'])
